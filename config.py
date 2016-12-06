3acc739f50df0c81c3c9f4346dd98354b7113d2b
Jiatao Gu <thomagram@gmail.com>
2016-12-06 16:02:54 -0500

    make pretraining easy

--- config.py
+++ config.py
@@ -3,5 +3,47 @@ Configuration for Simultaneous Neural Machine Translation
 """
 
 def pretrain_config():
-    pass
+
+    config = dict()
+
+    # training set (source, target)
+    config['datasets'] = ['/misc/kcgscratch1/ChoGroup/thoma_data/un16/train.un16.en-zh.zh.c0.tok.clean.bpe20k.np',
+                          '/misc/kcgscratch1/ChoGroup/thoma_data/un16/train.un16.en-zh.en.c0.tok.clean.bpe20k.np']
+
+    # validation set (source, target)
+    config['valid_datasets'] = ['/misc/kcgscratch1/ChoGroup/thoma_data/un16/devset.un16.en-zh.zh.c0.tok.bpe20k.np',
+                                '/misc/kcgscratch1/ChoGroup/thoma_data/un16/devset.un16.en-zh.en.c0.tok.bpe20k.np']
+
+    # vocabulary (source, target)
+    config['dictionaries']   = ['/misc/kcgscratch1/ChoGroup/thoma_data/un16/train.un16.en-zh.zh.c0.tok.clean.bpe20k.vocab.pkl',
+                                '/misc/kcgscratch1/ChoGroup/thoma_data/un16/train.un16.en-zh.en.c0.tok.clean.bpe20k.vocab.pkl']
+
+    # save the model to
+    config['saveto']      = '.pretraining/model_un16_bpe2k_uni_zh-en.npz'
+    config['reload_']     = True
+
+    # model details
+    config['dim_word']    = 512
+    config['dim']         = 1028
+    config['n_words']     = 20000
+    config['n_words_src'] = 20000
+
+    # learning details
+    config['decay_c']     = 0
+    config['clip_c']      = 1.
+    config['use_dropout'] = False
+    config['lrate']       = 0.0001
+    config['optimizer']   = 'adadelta'
+    config['patience']    = 1000
+    config['maxlen']      = 50
+    config['batch_size']  = 64
+    config['valid_batch_size'] =  64
+    config['validFreq']   = 1000
+    config['dispFreq']    = 50
+    config['saveFreq']    = 1000
+    config['sampleFreq']  = 99
+
+    return config
+
+
 
--- pretrain_uni.py
+++ pretrain_uni.py
@@ -1,48 +1,15 @@
-import numpy
-import os
-
 from nmt_uni import train
+from config import pretrain_config
 
 def main(job_id, params):
-    print params
-    validerr = train(saveto=params['model'][0],
-                     reload_=params['reload'][0],
-                     dim_word=params['dim_word'][0],
-                     dim=params['dim'][0],
-                     n_words=params['n-words'][0],
-                     n_words_src=params['n-words'][0],
-                     decay_c=params['decay-c'][0],
-                     clip_c=params['clip-c'][0],
-                     lrate=params['learning-rate'][0],
-                     optimizer=params['optimizer'][0],
-                     patience=1000,
-                     maxlen=50,
-                     batch_size=64,
-                     valid_batch_size=64,
-                     validFreq=1000,
-                     dispFreq=50,
-                     saveFreq=1000,
-                     sampleFreq=99,
-                     datasets=['/misc/kcgscratch1/ChoGroup/thoma_data/un16/train.un16.en-zh.zh.c0.tok.clean.bpe20k.np',
-                               '/misc/kcgscratch1/ChoGroup/thoma_data/un16/train.un16.en-zh.en.c0.tok.clean.bpe20k.np'],
-                     valid_datasets=['/misc/kcgscratch1/ChoGroup/thoma_data/un16/devset.un16.en-zh.zh.c0.tok.bpe20k.np',
-                                     '/misc/kcgscratch1/ChoGroup/thoma_data/un16/devset.un16.en-zh.en.c0.tok.bpe20k.np'],
-                     dictionaries=['/misc/kcgscratch1/ChoGroup/thoma_data/un16/train.un16.en-zh.zh.c0.tok.clean.bpe20k.vocab.pkl',
-                                   '/misc/kcgscratch1/ChoGroup/thoma_data/un16/train.un16.en-zh.en.c0.tok.clean.bpe20k.vocab.pkl'],
-                     use_dropout=params['use-dropout'][0])
+    print 'pretraining settings:'
+    for c, v in sorted(params.items(), key=lambda a:a[0]):
+        print '{}: {}'.format(c, v)
+
+    validerr = train(**params)
     return validerr
 
 if __name__ == '__main__':
-    main(0, {
-        'model': ['.pretraining/model_un16_bpe2k_uni_zh-en.npz'],
-        'dim_word': [512],
-        'dim': [1028],
-        'n-words': [20000],
-        'optimizer': ['adadelta'],
-        'decay-c': [0.],
-        'clip-c': [1.],
-        'use-dropout': [False],
-        'learning-rate': [0.0001],
-        'reload': [False]})
+    main(0, pretrain_config())
 
 
